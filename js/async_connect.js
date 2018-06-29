206c839cc8b25ce43766439b559830f8946b2075
5abarinath <sabarinath1996@gmail.com>
2018-06-30 01:44:50 +0530

    Layout pages are now dynamic

---
+++ js/async_connect.js
@@ -0,0 +1,29 @@
+function httpGetAsync(url, callback) {
+    if (window.XMLHttpRequest) {
+        xmlHttp = new XMLHttpRequest();
+    } else {
+        // code for older browsers
+        xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send(null);
+}
+
+function httpPostAsync(url, params, callback) {
+    if (window.XMLHttpRequest) {
+        xmlHttp = new XMLHttpRequest();
+    } else {
+        // code for older browsers
+        xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("POST", url, true);
+    xmlHttp.send(params);
+}
