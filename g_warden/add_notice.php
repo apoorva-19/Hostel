8eb2670a59d943ec4b92e1708fe7d492394d7d96
5abarinath <sabarinath1996@gmail.com>
2018-07-06 21:22:21 +0530

    Girls Warden can now add notices

---
+++ g_warden/add_notice.php
@@ -0,0 +1,32 @@
+<?php
+	require_once("../connect.php");
+	require_once("../convert.php");
+
+	if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['category']) && isset($_POST['startDate']) && isset($_POST['endDate']) && isset($_POST['noticeText']))
+    {
+		if(empty($_POST['category']) || empty($_POST['startDate']) || empty($_POST['endDate']) || empty($_POST['noticeText'])) {
+			echo "Fields cannot be empty";
+		}
+		else {
+			$category = $_POST['category'];
+			$startDate = $_POST['startDate'];
+			$endDate = $_POST['endDate'];
+			$noticeText = $_POST['noticeText'];
+
+			$convertedEndDate = convertDate($endDate);
+			$convertedStartDate = convertDate($startDate);
+
+			// echo $category." ".$convertedStartDate." ".$convertedEndDate." ".$noticeText;
+			
+			$stmt = $mysqli->prepare("INSERT INTO Notices (Category, Date_From, Notice_Text, Date_To) VALUES(?, ?, ?, ?)");
+			$stmt->bind_param("ssss", $category, $convertedStartDate, $noticeText, $convertedEndDate);
+			$stmt->execute();
+			$stmt->close();
+
+			echo "Success";
+		}
+	}
+	else {
+		echo "Fields cannot be empty";
+	}
+?>
